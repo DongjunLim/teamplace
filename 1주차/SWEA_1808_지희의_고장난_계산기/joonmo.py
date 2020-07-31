a73afd1f6642322805e473da4b0bf9eae613ecaa
jack9410 <46255505+jack9410@users.noreply.github.com>
2020-07-31 14:10:15 +0900

    Update joonmo.py

--- 1주차/SWEA_1808_지희의_고장난_계산기/joonmo.py
+++ 1주차/SWEA_1808_지희의_고장난_계산기/joonmo.py
@@ -1,2 +1,6 @@
+import sys
+sys.stdin = open("D:/GitHub/teamplace/teamplace/1주차/SWEA_1808_지희의_고장난_계산기/input.txt", "r")
+
 T = int(input())
 for test_case in range(1, T + 1):
+    print('#%d %d'%(test_case, 0))
