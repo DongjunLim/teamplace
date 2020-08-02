2f90a438945e741136897eef0fcdea8ab519546a
jack9410 <46255505+jack9410@users.noreply.github.com>
2020-08-02 23:43:15 +0900

    완료

--- 1주차/SWEA_1808_지희의_고장난_계산기/joonmo.py
+++ 1주차/SWEA_1808_지희의_고장난_계산기/joonmo.py
@@ -6,6 +6,8 @@ file_dr = os.path.abspath(__file__)
 curr_dr = os.path.dirname(file_dr)
 sys.stdin = open(curr_dr + "\input.txt", "r")
 
+# bfs 사용하서 풀이
+
 def get_divisor(num):
     divisors = []
     length = int(math.sqrt(num)) + 1
@@ -29,78 +31,78 @@ def check_input(num):
     return True
 
 def bfs(X):
+
+    # 모든 약수 구하기
     divisor_lst = get_divisor(X)
-    # print(divisor_lst)
+    # print(X)
+    # print('divisors = ', divisor_lst)
     
+
+    # 입력가능한 약수 구하기
     possible_divisors =[]
     
     for i in divisor_lst:
         if check_input(i):
             possible_divisors.append(i)
     
-    print(possible_divisors)
+    # print('poss divisors = ', possible_divisors)
     
+    if 1 in possible_divisors:
+        possible_divisors.remove(1)
+    
+    # print('poss divisors = ', possible_divisors)
+
+    # 입력가능 약수 없을때 -1 출력
     if len(possible_divisors) == 0:
         return -1
-    elif len(possible_divisors) == 1:
-        if 1 in possible_divisors:
-            return -1
+
+    # # 입력가능 약수 1개일때
+    # elif len(possible_divisors) == 1:
+    #     # 입력 가능한게 1밖에 없을때 -1 출력
+    #     if 1 in possible_divisors:
+    #         return -1
 
     ans_lst = []
+    # 숫자 바로 입력가능할 경우
     if X in possible_divisors:
         answer = len(str(X))+1
         return answer
+    
+    # 숫자 바로 입력 불가능한 경우
     else:
+        # 입력가능한 약수가 존재할 때
         for i in possible_divisors:
+            # 짝지은 약수도 입력가능할 때
             if X//i in possible_divisors:
                 answer = len(str(i)) + len(str(X//i)) + 2
                 return answer
+            # 짝지은 약수가 입력 불가능할 때
             else:
-                answer =  len(str(i)) + 1 + bfs(X//i)
-                ans_lst.append(answer)
-
-    print(ans_lst)
-    return min(ans_lst)
+                tmp = bfs(X//i)
+                if tmp == -1:
+                    return -1
+                else:
+                    answer =  len(str(i)) + 1 + tmp
+                    return answer
+    
+    # 답 경우의 수가 많을 때 더 적게 입력할 수 있는 경우 존재 여부?
+    # print(ans_lst)
+    # return min(ans_lst)
 
 T = int(input())
 for test_case in range(1, T + 1):
 
     avail_num = []
     button = list(map(int, input().split()))
+    # print(button)
     
-    for i in range(9,0,-1):
+    for i in range(9,-1,-1):
         if button[i]==1:
             avail_num.append(i)
-    print(avail_num)
+    # print('input able num = ', avail_num)
 
     X = int(input())
-    print(X)
-
-    # divisor_lst = get_divisor(X)
-    # print(divisor_lst)
-
-    # possible_divisors =[]
-    
-    # for i in divisor_lst:
-    #     if check_input(i):
-    #         possible_divisors.append(i)
-    
-    # print(possible_divisors)
-
-    # if len(possible_divisors) == 0:
-    #     answer = 0
-
-    # if X in possible_divisors:
-    #     answer = len(str(X))+1
-    # else:
-    #     for i in possible_divisors:
-    #         if X//i in possible_divisors:
-    #             answer = len(str(i)) + len(str(X//i)) + 2
-    #             break
-    #         else:
-    #             answer = 0
-
-
+    # print(X)
 
     print('#%d %d' % (test_case, bfs(X)))
 
