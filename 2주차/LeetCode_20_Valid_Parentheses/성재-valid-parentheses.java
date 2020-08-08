b96bcb703923be45a3923d8837d85ba4010f2b34
byeunsungjae <47214171+byeunsungjae@users.noreply.github.com>
2020-08-08 21:55:12 +0900

    Update and rename 성재-valid-parentheses to 성재-valid-parentheses.java

--- 2주차/LeetCode_20_Valid_Parentheses/성재-valid-parentheses.java
+++ 2주차/LeetCode_20_Valid_Parentheses/성재-valid-parentheses.java
@@ -5,6 +5,7 @@ package leetcode;
  * 닫는 괄호들이 들어오면 가장 최근에 넣은 괄호를 가져와서 비교하여 매칭되면 통과 아니면 실패
  * 모든 문자를 본후에 스텍에 무언가 남아있다면 실패
  * O(N)
+ * 1ms 37.5 MB
  */
 import java.util.*;
 public class valid_parentheses {
