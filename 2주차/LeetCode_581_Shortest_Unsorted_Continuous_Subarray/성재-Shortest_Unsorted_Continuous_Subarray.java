e7f9b52f56a00020a80a5e7e1ea015c26ba7351d
byeunsungjae <47214171+byeunsungjae@users.noreply.github.com>
2020-08-08 21:56:08 +0900

    Update 성재-Shortest_Unsorted_Continuous_Subarray.java

--- 2주차/LeetCode_581_Shortest_Unsorted_Continuous_Subarray/성재-Shortest_Unsorted_Continuous_Subarray.java
+++ 2주차/LeetCode_581_Shortest_Unsorted_Continuous_Subarray/성재-Shortest_Unsorted_Continuous_Subarray.java
@@ -3,6 +3,7 @@ package leetcode;
  * nums와 같은 배열을 만든 후 (arr) 소팅
  * 소팅후 원래의 배열 nums와 비교하여 바뀐점의 시작점과 마지막 점을찾음
  * 두값을 빼고 +1 시킨 값이 바꿔야할 값, 많약 바꾸지 않을시에는 0을 리턴
+ * 6ms 	40.8 MB
  */
 import java.util.*;
 public class shortest_unsorted_continuous_subarray {
