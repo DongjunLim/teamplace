97e7970914788171eb5204de03440805fdcde787
jack9410 <jack941028@naver.com>
2020-08-19 01:24:28 +0900

    암벽등반 수정 (27% 오답)

--- 3주차/BOJ_2412_암벽등반/joonmo.py
+++ 3주차/BOJ_2412_암벽등반/joonmo.py
@@ -9,11 +9,27 @@ def bfs(x,y):
     q = deque()
     q.append((x,y))
     visited[(x,y)] = 0
+    # bfs 시작 index
+    next_start_idx = 0
     while q:
         curr_x, curr_y = q.popleft()
-        for i in wall:
-            if i not in visited:
-                tmp_x, tmp_y = i
+        # 처음은 0부터 시작 그다음부터는 y좌표 2작은 곳부터 탐색
+        for i in range(next_start_idx, len(wall)):
+            tmp_x, tmp_y = wall[i]
+            
+            # y좌표 2작은 홈 인덱스 다음 탐색 시작부분으로 설정
+            if (curr_y - tmp_y) ==2:
+                next_start_idx = i
+            
+            # y좌표 2커지면 break
+            if (tmp_y - curr_y) > 2:
+                break
+
+            # y좌표 2보다 더 작으면 continue
+            if abs(curr_y - tmp_y) > 2:
+                continue
+
+            if wall[i] not in visited:
                 if abs(curr_x - tmp_x) <= 2 and abs(curr_y - tmp_y) <= 2:
                     if tmp_y == T:
                         return visited[(curr_x, curr_y)] + 1
